a240003e6ba1788cac07132b1bfa566371999b62
liuchenglong <liuchenglong125@foxmail.com>
2025-07-22 17:47:35 +0800

    2025-07-22 17:47:35

--- uuidv7-demo/src/main/java/com/seeseesea/Client.java
+++ uuidv7-demo/src/main/java/com/seeseesea/Client.java
@@ -2,6 +2,8 @@ package com.seeseesea;
 
 import com.github.f4b6a3.uuid.UuidCreator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -9,10 +11,18 @@ import java.util.UUID;
  */
 public class Client {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>(100);
         for (int i = 0; i < 100; i++) {
             UUID uuid = UuidCreator.getTimeOrderedEpoch();
-//            System.out.println(uuid.toString().replaceAll("-", ""));
-            System.out.println(uuid);
+            list.add(uuid.toString());
+        }
+
+        List<String> list2 = new ArrayList<>(List.copyOf(list));
+        list2.sort(String::compareTo);
+        for (int i = 0; i < 100; i++) {
+            System.out.printf("%s | %s%n", list.get(i), list2.get(i));
+            System.out.println(list.get(i).equals(list2.get(i)));
+
         }
     }
 }
