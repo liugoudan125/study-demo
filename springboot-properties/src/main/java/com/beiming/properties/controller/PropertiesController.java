2ea64ed96c18fa6d7fa5ea0b7ab70a76179eafae
liuchenglong <liuchenglong125@foxmail.com>
2025-08-06 14:09:44 +0800

    2025-08-06 14:09:43

--- springboot-properties/src/main/java/com/beiming/properties/controller/PropertiesController.java
+++ springboot-properties/src/main/java/com/beiming/properties/controller/PropertiesController.java
@@ -2,9 +2,12 @@ package com.beiming.properties.controller;
 
 import com.beiming.properties.properties.WhitelistProperties;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * PropertiesController
@@ -17,8 +20,17 @@ public class PropertiesController {
     @Resource
     private WhitelistProperties properties;
 
+    @Value("{test.list:1,2,3,4}")
+    private List<String> list;
+
     @GetMapping("get")
     public WhitelistProperties get() {
         return properties;
+    }
+
+
+    @GetMapping("list")
+    public List<String> getList() {
+        return list;
     }
 }
